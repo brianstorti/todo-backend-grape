66cae8376e382385bb211938a403a696f905b9ea
Brian Thomas Storti <btstorti@gmail.com>
2015-03-09 00:35:15 -0300

    use single quotes

--- test/test_todo_api.rb
+++ test/test_todo_api.rb
@@ -16,28 +16,28 @@ class TodoAPITest < MiniTest::Test
   end
 
   def test_get_root_returns_empty_list
-    get "/"
+    get '/'
     assert last_response.ok?
     assert_equal JSON.parse(last_response.body), []
   end
 
   def test_post_to_root_creates_new_todo
-    post "/", {title: 'foo'}
+    post '/', {title: 'foo'}
     assert last_response.created?
 
     created_todo = JSON.parse(last_response.body)
-    assert_equal 'foo', created_todo["title"]
-    assert_equal false, created_todo["completed"]
-    assert_match (/http:\/\/example.org\/\w/), created_todo["url"]
+    assert_equal 'foo', created_todo['title']
+    assert_equal false, created_todo['completed']
+    assert_match (/http:\/\/example.org\/\w/), created_todo['url']
   end
 
   def test_new_posts_have_an_url_that_can_be_followed
-    post "/", {title: 'foo'}
+    post '/', {title: 'foo'}
     assert last_response.created?
 
     created_todo = JSON.parse(last_response.body)
 
-    get created_todo["url"]
+    get created_todo['url']
     assert last_response.ok?
     retrieved_todo = JSON.parse(last_response.body)
 
@@ -45,10 +45,10 @@ class TodoAPITest < MiniTest::Test
   end
 
   def test_delete_todo
-    post "/", {title: 'foo'}
+    post '/', {title: 'foo'}
     assert last_response.created?
 
-    todo_url = JSON.parse(last_response.body)["url"]
+    todo_url = JSON.parse(last_response.body)['url']
 
     delete todo_url
 
@@ -57,29 +57,29 @@ class TodoAPITest < MiniTest::Test
   end
 
   def test_updates_an_existing_todo
-    post "/", {title: 'foo'}
+    post '/', {title: 'foo'}
     assert last_response.created?
 
-    todo_url = JSON.parse(last_response.body)["url"]
+    todo_url = JSON.parse(last_response.body)['url']
 
     patch todo_url, {title: 'updated'}
 
     get todo_url
     retrieved_todo = JSON.parse(last_response.body)
-    assert_equal 'updated', retrieved_todo["title"]
+    assert_equal 'updated', retrieved_todo['title']
   end
 
   def test_delete_all_todos
-    post "/", {title: 'foo'}
-    post "/", {title: 'foo2'}
+    post '/', {title: 'foo'}
+    post '/', {title: 'foo2'}
 
-    get "/"
+    get '/'
     todos = JSON.parse(last_response.body)
     assert_equal 2, todos.size
 
     delete '/'
 
-    get "/"
+    get '/'
     todos = JSON.parse(last_response.body)
     assert todos.empty?
   end
