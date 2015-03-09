adcc2ce48823d2db8388629c430d8e2c4e48c02a
Brian Thomas Storti <btstorti@gmail.com>
2015-03-08 22:03:04 -0300

    remove resource block

--- todo.rb
+++ todo.rb
@@ -14,56 +14,54 @@ class TodoAPI < Grape::API
     redirect '/todos'
   end
 
-  resource :todos do
-    options do
-      header 'Access-Control-Allow-Methods', 'GET,HEAD,POST,DELETE,OPTIONS,PUT'
-    end
+  options do
+    header 'Access-Control-Allow-Methods', 'GET,HEAD,POST,DELETE,OPTIONS,PUT'
+  end
 
+  get do
+    TodoRepo.todos
+  end
+
+  delete do
+    TodoRepo.delete_all
+  end
+
+  route_param :id do
     get do
-      TodoRepo.todos
+      todo = TodoRepo.find(params[:id])
+      error!('foo', 404) unless todo
+      todo
     end
+  end
 
+  params do
+    requires :id
+    optional :title, type: String
+    optional :status, type: String
+  end
+  patch do
+    begin
+      TodoRepo.update(params)
+    rescue RecordNotFound
+      error!("Record not found with id '#{params[:id]}'", 404)
+    end
+  end
+
+  route_param :id do
     delete do
-      TodoRepo.delete_all
-    end
-
-    route_param :id do
-      get do
-        todo = TodoRepo.find(params[:id])
-        error!('foo', 404) unless todo
-        todo
-      end
-    end
-
-    params do
-      requires :id
-      optional :title, type: String
-      optional :status, type: String
-    end
-    patch do
       begin
-        TodoRepo.update(params)
+        TodoRepo.delete(params[:id])
       rescue RecordNotFound
         error!("Record not found with id '#{params[:id]}'", 404)
       end
     end
+  end
 
-    route_param :id do
-      delete do
-        begin
-          TodoRepo.delete(params[:id])
-        rescue RecordNotFound
-          error!("Record not found with id '#{params[:id]}'", 404)
-        end
-      end
-    end
-
-    params do
-      requires :title, type: String
-    end
-    post do
-      params.merge!(completed: false)
-      TodoRepo.add(params)
-    end
+  params do
+    requires :title, type: String
+  end
+  post do
+    params.merge!(completed: false)
+    TodoRepo.add(params)
   end
 end
